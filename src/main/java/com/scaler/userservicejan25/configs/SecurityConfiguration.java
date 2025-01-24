940c3deb408af3c616d9be005e12ed2ae586820b
r7sh7 <bolinjkar.rishi@gmail.com>
2025-01-24 17:28:21 +0530

    Initialised authorization using OAuth

--- src/main/java/com/scaler/userservicejan25/configs/SecurityConfiguration.java
+++ src/main/java/com/scaler/userservicejan25/configs/SecurityConfiguration.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfiguration {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        try {
-            httpSecurity
-                    .authorizeHttpRequests((requests) -> requests
-                            .anyRequest().permitAll()
-                    )
-                    .cors(cors -> cors.disable())
-                    .csrf(csrf -> csrf.disable());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return httpSecurity.build();
-    }
-}
+//@Configuration
+//public class SecurityConfiguration {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        try {
+//            httpSecurity
+//                    .authorizeHttpRequests((requests) -> requests
+//                            .anyRequest().permitAll()
+//                    )
+//                    .cors(cors -> cors.disable())
+//                    .csrf(csrf -> csrf.disable());
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        return httpSecurity.build();
+//    }
+//}
